72abb71186acd14cda0871e057b781239042109c
HuzefaC <huzefachabukswar@gmail.com>
2022-04-26 18:06:38 +0530

    Added code to solve the test

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 def leap_year(year):
+  if year%4==0:
+    return True
   return False
--- test.py
+++ test.py
@@ -8,6 +8,9 @@ class Test(unittest.TestCase):
       Year is normal year if it is not divisible by 4, 100 and 400
     """
     self.assertFalse(main.leap_year(2001))
+  
+  def test_check_if_leap_year_only_divisible_by_4(self):
+    self.assertTrue(main.leap_year(1996))
 
 
 if __name__ == '__main__':
